c6c8cd8490b67f617369ed46e72cacb460acc1ed
wuufeii <wuufeii@qq.com>
2021-08-27 16:54:29 +0800

    bug: 结构调整

--- node/routes/followers.js
+++ node/routes/followers.js
@@ -10,13 +10,12 @@ const getUser = (params,fn) => {
   request(`https://github.com/${params.name}`,function(error,response,body){
     if (!error && response.statusCode === 200) {
       let obj = {}
-      let url = body.split('avatar avatar-user width-full border bg-white')[1].split('src="')[1].split('"')[0];
-      let user = body.split('<h1 class="vcard-names pl-2 pl-md-0">')[1].split('</h1>')[0];
-      let name = user.split('itemprop="name">')[1].split('</span>')[0]
-      let key = user.split('itemprop="additionalName">')[1].split('</span>')[0]
-      let followers = body.split('class="text-bold text-gray-dark"')[1].split('</span>')[0].split('>')[1].split('"')[0]
-      let following = body.split('class="text-bold text-gray-dark"')[2].split('</span>')[0].split('>')[1].split('"')[0]
-      let stars = body.split('class="text-bold text-gray-dark"')[3].split('</span>')[0].split('>')[1].split('"')[0]
+      let url = body.split('avatar avatar-user width-full border')[1].split('src="')[1].split('"')[0];
+      let name = body.split('itemprop="name">')[1].split('</span>')[0]
+      let key = body.split('itemprop="additionalName">')[1].split('</span>')[0]
+      let followers = body.split(`https://github.com/${params.name}?tab=followers`)[1].split('</span>')[0].split('<span')[1].split('>')[1]
+      let following = body.split(`https://github.com/${params.name}?tab=following`)[1].split('</span>')[0].split('<span')[1].split('>')[1]
+      let stars = body.split(`https://github.com/${params.name}?tab=stars`)[1].split('</span>')[0].split('<span')[1].split('>')[1]
       obj = {url,name,key,followers,following,stars}
       fn(obj)
     }
@@ -28,14 +27,19 @@ const getFollowing = (params,fn) => {
   request(`https://github.com/${params.name}?page=${params.page}&tab=${params.type}`,function(error,response,body){
     if (!error && response.statusCode === 200) {
       // let result = body.split('<a class="d-inline-block no-underline mb-1"');
-       let result = body.split('<div class="d-table table-fixed col-12 width-full py-4 border-bottom border-gray-light"');
+       let result = body.split('<div class="d-table table-fixed col-12 width-full py-4 border-bottom color-border-secondary"');
       let arr = []
       result.forEach((item,index) => {
         if(index!==0) {
           const url = item.split('src="')[1].split('"')[0]
-          const name = item.split('<span class="f4 link-gray-dark">')[1].split('</span')[0]
-          const hasKey = item.split('<span class="link-gray pl-1">')[1] === undefined ? item.split('<span class="link-gray">')[1]: item.split('<span class="link-gray pl-1">')[1]
-          const key = hasKey.split('</span')[0]
+          const hasName = item.split('<span class="f4 Link--primary">')[1].split('</span')
+          const name = hasName.length>0 ? hasName[0] : ''
+          // const key = item.split('<span class="Link--secondary pl-1">')[1].split('</span')[0]
+          // const hasKey1 = item.split('<span class="Link--secondary">')
+          // const hasKey2 = item.split('<span class="Link--secondary  pl-1">')
+          // const hasKey = hasKey2.length > 0 ? hasKey2 : hasKey1
+          // const key = hasKey[1].split('</span')
+          const key = ''
           const type = params.type
           arr.push({url,name,key,type})
         }
